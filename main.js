0b6bb0f85723a20bf8d37a6c8e3fd32132809c92
sasirat <sasirat019@gmail.com>
2019-03-29 22:55:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,48 @@
-const myform = document.querySelector('#need-validaton');
-const subject = document.querySelector('#subject');
-const email = document.querySelector('#email');
-const massage = document.querySelector('#massage');
-const name = document.querySelector('#name');
-const phone = document.querySelector('#phone');
+class contact {
+    constructor(subject,massage,name,gender,email,phone){
+        this.subject = subject;
+        this.massage = massage;
+        this.name = name;
+        this.gender = gender;
+        this.email = email;
+        this.phone = phone;
+    }
+}
 
+function saveContact(e){
+    e.preventDefault();
+
+    if(!validateForm(contact)){
+        return false;
+    }
+    
+    var contact_input = {
+        subject: subject,
+        massage: massage,
+        name: name,
+        gender: gender,
+        email: email,
+        phone: phone
+    }
+    console.log(contact_input);
+
+    if(localStorage.getItem('contacts') === ''){
+        var contacts = [];
+        contacts.push(contact_input);
+        localStorage.setItem('contacts',JSON.stringify(contacts));
+    }else{
+        var contacts = JSON.parse(localStorage.getItem('contacts'))
+        contacts.push(contact_input);
+        localStorage.setItem('contacts',JSON.stringify(contacts));
+    }
+    document.querySelector('#subject').value = '';
+    document.querySelector('#massage').value = '';
+    document.querySelector('#name').value = '';
+    document.querySelector('.gender').value = null;
+    document.querySelector('#email').value = '';
+    document.querySelector('#phone').value = '';
+
+    fetchContact();
+}
+
+function fetchContact();
